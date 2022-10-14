4f2b87d3f8656e689417ab98119feb79e6ae16ca
ryanq <ryanquay@outlook.com>
2022-10-14 13:58:26 -0700

    Added a properties class to hold Airbnb properties

---
+++ src/main/model/Properties.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Properties {
+    private List<Airbnb> properties;
+
+    public Properties() {
+        properties = new ArrayList<>();
+    }
+
+
+}
