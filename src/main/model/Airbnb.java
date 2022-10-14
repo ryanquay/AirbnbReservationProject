48ac4e0330917c9cb609df896c753ed4c80df5da
Ryan Quay <rquay@students.cs.ubc.ca>
2022-10-14 01:25:23 -0700

    Added a boolean method to check if the user can make a reservation

--- src/main/model/Airbnb.java
+++ src/main/model/Airbnb.java
@@ -3,6 +3,7 @@ package model;
 import java.util.ArrayList;
 import java.util.List;
 
+//Represents an Airbnb that has dates for reservation
 public class Airbnb {
     private static final int DAY_COUNT = 31;
     private List<String> reservations;
@@ -25,6 +26,25 @@ public class Airbnb {
             }
         }
         return displayInformation;
+    }
+
+    public boolean makeReservation(String name, int checkInDate, int checkOutDate) {
+        if (checkInDate < 1 || checkOutDate > DAY_COUNT || checkOutDate < checkInDate) {
+            System.out.println("Please choose appropriate check in and/or check out dates");
+            return false;
+        }
+        for (int i = checkInDate; i < checkOutDate; i++) {
+            if (reservations.get(i) == name) {
+                System.out.println("You have already reserved at least one of your chosen dates. \n"
+                        + "Please choose a timeframe of available days");
+                return false;
+            } else if (reservations.get(i) != null) {
+                System.out.println("At least one of your chosen days have been reserved by another person. \n"
+                        + "Please choose a timeframe of available days");
+                return false;
+            }
+        }
+        return true;
     }
 
     public List<String> getReservations() {
