25e16c2f6bb85ece250c81de578308405c4763bb
Ryan Quay <ryanquay@outlook.com>
2022-10-17 00:37:47 -0700

    Made methods to display login menu and set up an initial property

--- src/main/ui/AirbnbApp.java
+++ src/main/ui/AirbnbApp.java
@@ -1,14 +1,54 @@
 package ui;
 
 
+import model.Airbnb;
+import model.Properties;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class AirbnbApp {
+    private Scanner input;
+    private String customerName;
+    private String airbnbName;
+    private Properties propertyList;
+    //private Airbnb airbnb;
+
 
     public AirbnbApp() {
         runAirbnbApp();
     }
 
     private void runAirbnbApp() {
+        boolean keepGoing = true;
+        String command = null;
+
+        while (keepGoing) {
+            displayLoginMenu();
+            command = input.next();
+
+
+            if (command.equalsIgnoreCase("q")) {
+                keepGoing = false;
+            } else {
+                processCommand(command);
+            }
+        }
+    }
+
+    private void initialSetUp() {
+        propertyList = new Properties();
+        propertyList.addProperties(new Airbnb("House1"));
+    }
+
+    private void displayLoginMenu() {
+        System.out.println("Welcome! Please login as: ");
+        System.out.println("a -> Admin");
+        System.out.println("c -> Customer");
+        System.out.println("q -> quit");
+    }
+
+    private void processCommand(String command) {
 
     }
 }
