6556436670ac055c2cf9785f8402d2b2efe04a00
Ryan Quay <ryanquay@outlook.com>
2022-10-26 00:42:21 -0700

    Implemented writer that can write JSON representation of Properties to file

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,5 +1,35 @@
 package persistence;
 
-public class JsonWriter {
+import model.Properties;
+import org.json.JSONObject;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+public class JsonWriter {
+    private static final int SPACING = 4;
+    private PrintWriter writer;
+    private String fileLocation;
+
+    public JsonWriter(String fileLocation) {
+        this.fileLocation = fileLocation;
+    }
+
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter(new File(fileLocation));
+    }
+
+    public void write(Properties pp) {
+        JSONObject json = pp.toJson();
+        saveToFile(json.toString(SPACING));
+    }
+
+    public void close() {
+        writer.close();
+    }
+
+    public void saveToFile(String json) {
+        writer.print(json);
+    }
 }
