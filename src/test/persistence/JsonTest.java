b22ef7d28824ecf2fb388722d8c2d1baf0c5aa65
Ryan Quay <ryanquay@outlook.com>
2022-10-27 21:25:07 -0700

    Added test cases to write data with one or more properties with reservations

---
+++ src/test/persistence/JsonTest.java
@@ -0,0 +1,15 @@
+package persistence;
+
+import model.Airbnb;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonTest {
+
+    protected void checkProperties(String name, List<String> reservations, Airbnb airbnb) {
+        assertEquals(name,airbnb.getAirbnbName());
+        assertEquals(reservations,airbnb.getReservations());
+    }
+}
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,15 +1,16 @@
 package persistence;
 
+import model.Airbnb;
 import model.Properties;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class JsonWriterTest {
+public class JsonWriterTest extends JsonTest{
 
 
     @Test
@@ -39,6 +40,53 @@ public class JsonWriterTest {
             fail("Exception should not have been thrown");
         }
     }
+
+    @Test
+    void TestWriterWithProperties() {
+        try {
+            Properties pp = new Properties();
+            Airbnb house1= new Airbnb("House1");
+            Airbnb house2 = new Airbnb("House2");
+            pp.addProperties(house1);
+            pp.addProperties(house2);
+            JsonWriter writer = new JsonWriter("./data/testWriterWithProperties.json");
+            writer.open();
+            writer.write(pp);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterWithProperties.json");
+            pp=reader.read();
+            List<Airbnb> houses = pp.getProperties();
+            assertEquals(2,houses.size());
+            checkProperties(house1.getAirbnbName(),house1.getReservations(),houses.get(0));
+            checkProperties(house2.getAirbnbName(),house2.getReservations(),houses.get(1));
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void TestWriterWithPropertiesWithReservations() {
+        try {
+            Properties pp = new Properties();
+            Airbnb house1= new Airbnb("House1");
+            house1.makeReservation("Ryan", 2, 5);
+            pp.addProperties(house1);
+            JsonWriter writer = new JsonWriter("./data/testWriterWithPropertiesWithReservations.json");
+            writer.open();
+            writer.write(pp);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterWithPropertiesWithReservations.json");
+            pp=reader.read();
+            List<Airbnb> houses = pp.getProperties();
+            assertEquals(1,houses.size());
+            checkProperties(house1.getAirbnbName(),house1.getReservations(),houses.get(0));
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
 
 
 
